855c1b1e3f37fbada239863e17d355e464d0cb01
suchal <suchalriaz@gmail.com>
2016-12-07 13:28:19 +0000

    contact-us form v2

--- contact/index.php
+++ contact/index.php
@@ -3,9 +3,14 @@
 namespace Contact;
 require_once(__DIR__."/../bootstrap.php");
 
-use Respect\Validation as v;
+var_dump([$_POST, $_GET]);
+
+use App\OlMail;
+use Respect\Validation\Validator as v;
+
 
 if($_SERVER['REQUEST_METHOD'] == "POST"){
+	$errors=[];
 	if(!strlen($_POST['name'] ?? ""))
 		$errors['name'] = "Please enter a name!";
 	else
@@ -19,7 +24,7 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
 	else
 		$message = $_POST['message'];
 	$emailvalidation = v::NotEmpty()->email();
-	if(!$emailvalidation->validate($email))
+	if(!$emailvalidation->validate($email??''))
 		$errors['email'] = "Please enter a valid email address";
  	// $ipaddress = \App\get_client_ip();
  	// $captcha = \App\send_post("https://www.google.com/recaptcha/api/siteverify", 
@@ -33,7 +38,7 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
 
 	if(!count($errors)){
 		//first populate the messages table
-		$stmt = $conn->preapre("insert into messages(name, email, message) values(?,?,?)");
+		$stmt = $conn->prepare("insert into messages(name, email, message) values(?,?,?)");
 		$stmt->bind_param("sss", $name, $email, $message);
 		$stmt->execute();
 		//then send the email to web_it@nustolympiad.com
@@ -48,7 +53,7 @@ $txtmessage
 </body>
 </html>
 htmlmessage;
-		$mail = new OlMail(["name"=>"Web&IT Olympiad", "email"=>"web_it@nustolympiad.com"], $htmlmessage, $txtmessage);
+		$mail = new OlMail(["name"=>"Web&IT Olympiad", "email"=>"web_it@nustolympiad.com"],"A new message!", $htmlmessage, $txtmessage);
 		$mail->send();
 	}
 
@@ -57,4 +62,5 @@ if(isset($errors) && count($errors)){
 	foreach($errors as $field=>$error){
 		echo $error;
 	}
-} 
+}
+\App\redirect("/");
