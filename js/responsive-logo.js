a67554bc4c88a5254c60287aa1886ce3f3156907
mukarram189 <mukarramishaq189@gmail.com>
2016-12-20 20:38:29 +0500

    just home page settings

--- js/responsive-logo.js
+++ js/responsive-logo.js
@@ -23,7 +23,7 @@ $(document).ready(function(){
 
 		}
 		else{
-			logoDiv.className = "col-md-6 col-md-offset-4 col-xs-12 col-sm-6 col-sm-offset-3 text-center homepageleft";
+			logoDiv.className = "col-lg-4 col-lg-offset-4 col-md-6 col-md-offset-4 col-xs-12 col-sm-6 col-sm-offset-3 text-center homepageleft";
 		}
 	}
 	adjustLogo();
--- js/timeline.js
+++ js/timeline.js
@@ -3,17 +3,23 @@ $(document).ready(function(){
 	function addMarginToEventsTimeline(){
 		var width = $(window).width();
 		var marginLeft, marginRight, minWidth;
-		if(width >= 1002){
-			minWidth = 1002;
+		if(width >= 1500){
+			minWidth = 1500;
 			marginLeft = Math.floor((width-minWidth)/4);
 			marginRight = Math.floor((width-minWidth)/4);
-			$('.class-desktop').css({"margin-left":marginLeft+"px"});
+			/*$('.class-desktop').css({"margin-left":marginLeft+"px"});*/
+		}
+		else if(width >= 1002){
+			minWidth = 1002;
+			marginLeft = Math.floor((width-minWidth)/2);
+			marginRight = Math.floor((width-minWidth)/2);
+			/*$('.class-desktop').css({"margin-left":marginLeft+"px"});*/
 		}
 		else if(width >= 900){
 			minWidth = 900;
 			marginLeft = Math.floor((width+60-minWidth)/4);
 			marginRight = Math.floor((width+60-minWidth)/4);
-			$('.class-desktop').css({"margin-left":marginLeft+"px"});
+			/*$('.class-desktop').css({"margin-left":marginLeft+"px"});*/
 		}
 		
 	}
